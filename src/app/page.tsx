f613d57de9b0f9b6980be574b318056042f3e6bd
Ashutosh Kumar <ashutoshkumar@Ashutoshs-Air.lan>
2024-11-28 20:10:06 -0600

    inital commit

---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import Navbar from "react-bootstrap/Navbar";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Image from 'next/image';
+import logo from '../../public/OfficerLibraryLogoWhite.jpeg';
+import mainImage from '../../public/OFL_main_image.jpeg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navigation = () => {
+  return (
+    <>
+      <Navbar>
+        <Container>
+            <Navbar.Brand href="#home">
+                <Image
+                 src={logo}
+                 width={50}
+                 height={50}
+                 alt= "Officer's
+                 Library"
+               />
+            </Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+        </Container>
+      </Navbar>
+      <div>
+      <Image
+       src={mainImage}
+         width={0}
+         height={0}
+       className="w-full h-30"
+       alt= "Officer's Library"
+     />
+     </div>
+    </>
+  );
+};
+
+export default Navigation;
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      backgroundImage: {
+        'office': "url('../../public/Officers_Library_Logo.svg')",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
